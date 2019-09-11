10963afaefe8e8c32ed01e723484525da30f1e54
dhrumil <dhrumil.shah@gmail.com>
2019-09-11 21:33:33 +0530

    70-ClimbingStairs.py with memonization

--- 70-ClimbingStairs.py
+++ 70-ClimbingStairs.py
@@ -2,7 +2,7 @@
 70. Climbing Stairs : interesting fibonacci series questions
 """
 class Solution:
-    def climbStairs(self, n: int) -> int:
+    def climbStairs_fib(self, n: int) -> int:
         if (n ==1):
             return 1
         
@@ -15,9 +15,19 @@ class Solution:
         
         return second
     
-    def climbStairs_memo(self, n: int) -> int:
-        memo = [0]* n+1
+    def climbStairs(self, n: int) -> int:
+        memo = [0] * (n+1)
         return self.climb_stairs(0, n, memo)
     
     def climb_stairs(self, i: int, n: int, memo = []) -> int:
+        if i > n:
+            return 0
+        if i ==n:
+            return 1
+        if memo[i] > 0:
+            return memo[i]
+        
+        memo[i] = self.climb_stairs(i+1, n , memo) + self.climb_stairs(i+2, n , memo)
+        
+        return memo[i]
         return 0
